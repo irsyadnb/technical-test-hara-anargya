b261039881bd59fd02c0bc2ba1fc55d17963aab1
irsyadnb <irsyadxd@gmail.com>
2024-02-17 23:12:20 +0700

    feat: action button

---
+++ src/components/privates/ActionButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["id", "text"]);
+const emit = defineEmits(["actionButton"]);
+
+const handleButtonClick = (id) => {
+  console.log('clicked on action button component')
+  emit("actionButton", id);
+};
+</script>
+
+<template>
+  <button class="p-1 shadow-md font-semibold" @click="handleButtonClick(id)">
+    {{ text }}
+  </button>
+</template>
